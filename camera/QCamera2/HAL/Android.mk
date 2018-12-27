cbf0ee778384b3c6378dd8103c0787482e83e148
Inderpreet Singh <inderpreetbhangu2@gmail.com>
2018-12-27 05:42:11 +0000

    LS-5015: Added AOSP Changes
    Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -48,10 +48,10 @@ LOCAL_CFLAGS += -DDEFAULT_DENOISE_MODE_ON
 LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/../stack/common \
     frameworks/native/include/media/openmax \
-    $(call project-path-for,qcom-display)/libgralloc \
-    $(call project-path-for,qcom-display)/libqdutils \
-    $(call project-path-for,qcom-display)/libqservice \
-    $(call project-path-for,qcom-media)/libstagefrighthw \
+    hardware/qcom/display-caf/msm8916/libgralloc \
+    hardware/qcom/display-caf/msm8916/libqdutils \
+    hardware/qcom/display-caf/msm8916/libqservice \
+    hardware/qcom/media-caf/msm8916/libstagefrighthw \
     system/media/camera/include \
     $(LOCAL_PATH)/../../mm-image-codec/qexif \
     $(LOCAL_PATH)/../../mm-image-codec/qomx_core \
