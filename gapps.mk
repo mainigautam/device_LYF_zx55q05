c992e636028727850c627c19044f871f7eacee6d
Mohd Faraz <mohd.faraz.abc@gmail.com>
2019-01-01 18:53:26 +0530

    LS-5015: gapps.mk: exclude messaging

--- gapps.mk
+++ gapps.mk
@@ -2,8 +2,8 @@
 GAPPS_VARIANT := nano
 $(call inherit-product-if-exists, vendor/opengapps/build/opengapps-packages.mk)
 
-GAPPS_PRODUCT_PACKAGE += Chrome Drive Gmail
+GAPPS_PRODUCT_PACKAGE += Chrome Drive Gmail  
 GAPPS_FORCE_BROWSER_OVERRIDES := true
-GAPPS_BYPASS_PACKAGE_OVERRIDES := GoogleCamera
+GAPPS_BYPASS_PACKAGE_OVERRIDES := GoogleCamera messaging
 GAPPS_FORCE_PACKAGE_OVERRIDES := true
 GAPPS_EXCLUDED_PACKAGES := GoogleCamera
