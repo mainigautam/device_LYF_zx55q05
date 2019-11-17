0f795900d1fd4786bd22d57f0d383e962bb96afa
Gautam <gautammaini9@gmail.com>
2019-11-17 14:07:24 +0000

    bluetooh: fix branding and confirm functioning

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Nubia Z9 mini"
+#define BTM_DEF_LOCAL_NAME   "LS-5504"
 #define BLUETOOTH_QTI_SW TRUE
 
 #endif
